b40ba6b3d7f7879cd066531a851400035bb92d10
fasteque <info@fasteque.com>
2015-05-08 22:54:32 +0200

    Enabled LeakCanary

---
+++ app/src/main/java/com/fasteque/hooray/HoorayApplication.java
@@ -0,0 +1,25 @@
+package com.fasteque.hooray;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
+
+/**
+ * Created by danielealtomare on 08/05/15.
+ */
+public class HoorayApplication extends Application {
+
+    private RefWatcher refWatcher;
+
+    public static RefWatcher getRefWatcher(Context context) {
+        HoorayApplication application = (HoorayApplication) context.getApplicationContext();
+        return application.refWatcher;
+    }
+
+    @Override public void onCreate() {
+        super.onCreate();
+        refWatcher = LeakCanary.install(this);
+    }
+}
