668b395e7e30c926724a70334297434e390c6d51
fasteque <info@fasteque.com>
2015-05-11 21:52:44 +0200

    dummy fixture activity implementation

--- app/src/main/java/com/fasteque/hooray/activities/FixtureActivity.java
+++ app/src/main/java/com/fasteque/hooray/activities/FixtureActivity.java
@@ -1,7 +1,44 @@
 package com.fasteque.hooray.activities;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.fasteque.hooray.R;
+import com.fasteque.hooray.presenters.FixturePresenter;
+import com.fasteque.hooray.views.FixtureView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 /**
  * Created by danielealtomare on 05/05/15.
  */
-public class FixtureActivity extends BaseActivity {
+public class FixtureActivity extends BaseActivity implements FixtureView {
+    private FixturePresenter fixturePresenter;
+
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fixture);
+
+        ButterKnife.inject(this);
+        setSupportActionBar(toolbar);
+
+        fixturePresenter = new FixturePresenter(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        fixturePresenter.onResume();
+    }
+
+    @Override
+    public Context getContext() {
+        return this;
+    }
 }
